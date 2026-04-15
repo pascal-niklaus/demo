2f015ead435bf8fe1b0e568a8e2bcdd322b9d494
Pascal Niklaus <pascal.niklaus@ieu.uzh.ch>
2026-04-15 10:23:58 +0200

    file1.R created

---
+++ src/file1.R
@@ -0,0 +1,12 @@
+######################################################################
+###
+### Silly demo code
+###
+### Pascal A. Niklaus
+###
+### History:
+### - 15-04-2026 file created
+
+options(digits=3)
+close_all()
+rm(list=ls())
