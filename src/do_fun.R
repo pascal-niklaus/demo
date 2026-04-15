6d1dee36352667ab1a4bf17797790b612c204d7c
my-other-self <pascal-other@niklaus.org>
2026-04-15 13:57:42 +0200

    comments added

--- src/do_fun.R
+++ src/do_fun.R
@@ -2,11 +2,15 @@
 ###
 ### nonsensical demo functions
 
+## a funny function
+
 do_fun <- function(n) {
     for (i in seq_len(n)) {
         cat("This is fun!\n")
     }
 }
+
+## an even more funny function
 
 more_fun <- function(n) {
     for (i in seq_len(n)) {
